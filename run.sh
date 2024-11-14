c526c8deb44a5b8e20bd0de9a14893459dc3d2f7
yinming <371695272@qq.com>
2024-11-14 23:03:58 +1100

    20241114_add  eg

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-
-python3 ./src/main.py
-python3 ./src/q_a.py > q_a_print.txt
-python3 ./src/q_b.py > q_b_print.txt
-python3 ./src/q_c.py > q_c_print.txt
+cd src
+python3 main.py
+python3 q_a.py > ../q_a_print.txt
+python3 q_b.py > ../q_b_print.txt
+python3 q_c.py > ../q_c_print.txt
